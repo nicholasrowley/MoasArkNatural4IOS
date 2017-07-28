44e7eab407bf4774a7aed62d08c507393d75988d
Nicholas Rowley <nicholas@Nicholass-iMac.local>
2017-07-28 23:34:44 +1200

    app toolbar icons (in progress)

--- MoasArkNatural/ViewController.swift
+++ MoasArkNatural/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var homeToolbar: UIToolbar!
+    @IBOutlet weak var placeholderLabel: UILabel!
+    var phNumber = 0
+    
+    @IBOutlet weak var imageButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Create items for the toolbar
+        let item1 = UIBarButtonItem(title: "Start", style: UIBarButtonItemStyle.plain,
+                                    target: self, action: #selector(startAction))
+        
+        let toolbarLogoImage = UIImage(named: "Logo")
+        let toolbarLogo = UIBarButtonItem(image: toolbarLogoImage?.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action:  #selector(logoOnTap))
+        
+        //Add to items collection
+        homeToolbar.items?.append(item1)
+        homeToolbar.items?.insert(toolbarLogo, at: 0)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func startAction(barButtonItem: UIBarButtonItem) {
+        placeholderLabel.text = "Start action \(phNumber)"
+        phNumber += 1
+    }
+    
+    func logoOnTap(_ sender: UIBarButtonItem)
+    {
+        
+    }
 
+    @IBAction func itemAction(_ sender: UIBarButtonItem) {
+        placeholderLabel.text = "Start action \(phNumber)"
+        phNumber += 1
+    }
+    
 
 }
 
