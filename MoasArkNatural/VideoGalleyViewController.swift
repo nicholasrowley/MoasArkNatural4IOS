c4168b8caa207c4b1235addbc7edc3f35dea59bd
Nicholas Rowley <nicholas@Nicholass-iMac.local>
2017-07-30 00:22:37 +1200

    Added Navigation controllers and toolbar options.

---
+++ MoasArkNatural/VideoGalleyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VideoGalleyViewController.swift
+//  MoasArkNatural
+//
+//  Created by Nicholas Rowley on 29/07/17.
+//  Copyright © 2017 One Life Group (NZ) Limited. All rights reserved.
+//
+
+import UIKit
+
+class VideoGalleyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MoasArkNatural/ViewController.swift
+++ MoasArkNatural/ViewController.swift
@@ -18,16 +18,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Navigation bar
+        //create a new button
+        let button = UIButton.init(type: .custom)
+        //set image for button
+        button.setImage(UIImage(named: "Logo"), for: UIControlState.normal)
+        //add function for button
+        button.addTarget(self, action: #selector(logoOnTap(_:)), for: UIControlEvents.touchUpInside)
+        //set frame
+        button.frame = CGRect(x: 0, y: 0, width: 53, height: 51)
+        
+        let barButton = UIBarButtonItem(customView: button)
+        //assign button to navigationbar
+        self.navigationItem.leftBarButtonItem = barButton
+        
+        
         //Create items for the toolbar
-        let item1 = UIBarButtonItem(title: "Start", style: UIBarButtonItemStyle.plain,
-                                    target: self, action: #selector(startAction))
+        let notificationsBarItem = UIBarButtonItem(title: "Notifications", style: UIBarButtonItemStyle.plain, target: self, action: #selector(notificationsAction))
         
-        let toolbarLogoImage = UIImage(named: "Logo")
-        let toolbarLogo = UIBarButtonItem(image: toolbarLogoImage?.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action:  #selector(logoOnTap))
+        let danceOrExBarItem = UIBarButtonItem(title: "Dance or Exercise video gallery", style: UIBarButtonItemStyle.plain, target: self, action: #selector(danceOrExAction))
         
-        //Add to items collection
-        homeToolbar.items?.append(item1)
-        homeToolbar.items?.insert(toolbarLogo, at: 0)
+        let foodBarItem = UIBarButtonItem(title: "Food video gallery", style: UIBarButtonItemStyle.plain, target: self, action: #selector(foodAction))
+        
+        //Add to items collection in order
+        homeToolbar.items?.append(notificationsBarItem)
+        homeToolbar.items?.append(danceOrExBarItem)
+        homeToolbar.items?.append(foodBarItem)
+        
         
     }
 
@@ -36,18 +53,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func startAction(barButtonItem: UIBarButtonItem) {
-        placeholderLabel.text = "Start action \(phNumber)"
-        phNumber += 1
-    }
     
+    //All toolbar onTap functions
     func logoOnTap(_ sender: UIBarButtonItem)
     {
         
     }
+    
+    func notificationsAction(barButtonItem: UIBarButtonItem) {
+        placeholderLabel.text = "Notifications action \(phNumber)"
+        phNumber += 1
+    }
+    
+    func danceOrExAction(barButtonItem: UIBarButtonItem) {
+        placeholderLabel.text = "Dance or Exercise video gallery action \(phNumber)"
+        phNumber += 1
+    }
+    
+    func foodAction(barButtonItem: UIBarButtonItem) {
+        placeholderLabel.text = "Food video gallery action \(phNumber)"
+        phNumber += 1
+    }
 
+    //TODO will delete at some point
     @IBAction func itemAction(_ sender: UIBarButtonItem) {
         placeholderLabel.text = "Start action \(phNumber)"
+        phNumber += 1
+    }
+    
+    @IBAction func goToMoasArkShop(_ sender: UIButton) {
+        //go to moas ark shop
+        let url = URL(string: "https://moasarknaturalnz.com/en_NZ/shop/")!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    
+    @IBAction func refreshAction(_ sender: UIBarButtonItem) {
+        placeholderLabel.text = "Refresh action \(phNumber)"
+        phNumber += 1
+    }
+    
+    
+    @IBAction func searchAction(_ sender: UIBarButtonItem) {
+        placeholderLabel.text = "Search action \(phNumber)"
         phNumber += 1
     }
     
